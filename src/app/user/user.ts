786e69f9ac79da1932267633709b0fc1df65c252
taektu_coding <taektu.coding@gmail.com>
2025-07-17 10:19:42 +0200

    added: User list

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
-  styleUrl: './user.scss'
+  styleUrl: './user.scss',
 })
 export class User {
+  @Input() avatar!: string;
+  @Input() name!: string;
 
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {}
 }
