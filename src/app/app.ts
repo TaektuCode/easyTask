0cbd5cd3444ababb497258a9cb791d7212c5e078
taektu_coding <taektu.coding@gmail.com>
2025-07-17 13:22:06 +0200

    feat: Add Tasks component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,23 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { User } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from './tasks/tasks';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, User],
+  imports: [RouterOutlet, Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('easyTask');
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
