85809dbc229d74045bd03fea5a6539dc6c4c71f0
taektu_coding <taektu.coding@gmail.com>
2025-07-18 10:15:37 +0200

    refactor(user): Consolidate inputs into a single user object

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+interface UserInterface {
+  id: string;
+  avatar: string;
+  name: string;
+}
 @Component({
   selector: 'app-user',
   imports: [],
@@ -6,17 +12,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.scss',
 })
 export class User {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: UserInterface;
 
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
