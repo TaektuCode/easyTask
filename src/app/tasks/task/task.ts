a16245ab2a7a5c3b4e6e5c9914208816c35601f9
taektu_coding <taektu.coding@gmail.com>
2025-07-20 15:10:08 +0200

    feat(task): outputting the task data

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface TaskInterface {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 
 @Component({
   selector: 'app-task',
   imports: [],
   templateUrl: './task.html',
-  styleUrl: './task.scss'
+  styleUrl: './task.scss',
 })
 export class Task {
-
+  @Input({ required: true }) task!: TaskInterface;
 }
