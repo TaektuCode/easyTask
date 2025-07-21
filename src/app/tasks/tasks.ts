7d4eac124583ac75a3964940dae3abcb171ef287
taektu_coding <taektu.coding@gmail.com>
2025-07-21 09:45:29 +0200

    feat(tasks): Implement add new task UI

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,15 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Task } from './task/task';
+import { NewTask } from './new-task/new-task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [Task],
+  imports: [Task, NewTask],
   templateUrl: './tasks.html',
   styleUrl: './tasks.scss',
 })
 export class Tasks {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+
+  isAddingTask = false;
+
   tasks = [
     {
       id: 't1',
@@ -42,5 +46,9 @@ export class Tasks {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
